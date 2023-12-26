6793a4245c1c013381684c74d7707e6dfb65aa7f
Jean Pierre Cardenas Perea <jpcardenas190@hotmail.com>
2023-12-26 10:56:09 -0500

    fix responsive product page

--- app/page.js
+++ app/page.js
@@ -5,10 +5,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="relative w-screen max-w-[1440px] min-h-screen bg-blue-50 px-5 lg:px-20">
-      <div className="relative w-full flex flex-col gap-14 h-fit m-0 py-12">
+    <main className="relative w-screen max-w-[1440px] min-h-screen bg-blue-50 px-5 md:px-12 lg:px-20">
+      <div className="relative w-full flex flex-col gap-14 h-fit m-0 py-6 lg:py-12">
         <Image src={logo} height={80} alt="logo-image.png" /> 
-        <h1 className="font-poppins font-semibold text-3xl lg:text-7xl text-blue-500 uppercase tracking-wider">
+        <h1 className="font-poppins font-semibold text-3xl md:text-5xl lg:text-7xl md:max-w-[800px] lg:max-w-none text-blue-500 uppercase tracking-wider">
           Unlocking the Power of Premium Names for Your Pharmaceutical Ventures
         </h1>
       </div>
@@ -22,7 +22,7 @@ export default function Home() {
             />
           ))}
         </div>
-        <div className="h-fit flex flex-1 w-full lg:max-w-[365px]">
+        <div className="h-fit flex flex-1 w-full md:max-w-[500px] lg:max-w-[365px]">
           <p className="relative font-poppins text-blue-600 text-sm lg:text-base tracking-wider">
             Explore our curated collection of premium domain names meticulously
             selected to elevate your pharma brand. Each domain is a unique
--- app/product/page.js
+++ app/product/page.js
@@ -4,22 +4,22 @@ import Image from "next/image";
 
 const page = () => {
   return (
-    <section className="w-screen h-screen bg-white flex">
-      <div className="w-2/4 h-screen relative flex flex-col p-16 gap-10">
+    <section className="w-screen h-screen bg-white flex flex-col lg:flex-row">
+      <div className="w-full lg:w-2/4 h-fit lg:h-screen relative flex flex-col p-5 md:p-10 lg:p-16 lg:gap-10">
         <ReturnButton />
         <div className="relative h-full flex flex-col justify-between">
-          <div className="relative h-[300px] w-full overflow-hidden flex justify-center items-center">
+          <div className="relative h-[200px] lg:h-[300px] w-full overflow-hidden flex justify-center items-center">
             <Image src={logo1} height={300} alt="product-image" />
           </div>
           <a
             href="https://www.namebuzzard.com/product-category/all-categories/?product_tag=cannabis"
-            className="w-full h-[90px] bg-blue-500 rounded-3xl font-poppins font-medium text-xl text-white flex justify-center items-center"
+            className="w-full h-[60px] lg:h-[90px] bg-blue-500 rounded-xl lg:rounded-3xl font-poppins font-medium text-xl text-white flex justify-center items-center"
           >
             Shop Now
           </a>
         </div>
       </div>
-      <div className="w-2/4 h-screen bg-blue-800 flex flex-col p-16 gap-12">
+      <div className="w-full lg:w-2/4 h-fit lg:h-screen bg-blue-800 flex flex-col px-5 py-10 md:p-10 lg:p-16 gap-12">
         <div className="relative flex flex-col gap-3">
           <h5 className="font-poppins text-white text-xl font-semibold">
             DOMAIN NAME
